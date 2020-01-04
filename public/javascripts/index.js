692f3cbb2ac95d75393eacdb4514c696a07e4348
Marc Bernasconi <steelstring@protonmail.com>
2020-01-03 20:02:04 -0800

    deleted old code

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,21 +9,7 @@ $(function () {
     return false
   })
 
-  //$('select#movieDate').val( '2017-10-05' )
   $('select#movieDate').change()
-
-/*
-  $('a.chooseDate').click( function() {
-    const date = $(this).attr( 'data-date' )
-    $('table.listings tbody tr').hide()
-    $('table.listings tbody tr').has('td:nth-child(1):contains('+date+')').show()
-    const dateObj = new Date( date )
-    $('span.selectedDate').text( dateObj.toUTCString().substr( 0, 16 ) )
-    return false
-  })
-*/
-
-  //$('a.chooseDate[data-date=]').click( function() {
 })
 
 
