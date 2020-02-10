b5d375c6d5193357176f102ba020aaeae3b76052
Marc Bernasconi <marc.p.bernasconi@macys.com>
2020-02-10 15:29:59 -0800

    fixed most eslint warnings

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-var express = require('express')
-var session = require('express-session')
-var path = require('path')
+var express = require('express');
+var session = require('express-session');
+var path = require('path');
 //var favicon = require('serve-favicon')
-var logger = require('morgan')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var index = require('./routes/index')
+var index = require('./routes/index');
 
-var app = express()
+var app = express();
 
 // view engine setup
 if (app.get('env') === 'development') {
-  app.locals.pretty = true
+  app.locals.pretty = true;
 }
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   resave: false,
   saveUninitialized: false,
   secret: 'dfaf87y8Y*&df8798iavoadfv',
-}))
-app.use(require('connect-flash')())
+}));
+app.use(require('connect-flash')());
 
-app.use('/', index)
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use(function(err, req, res) {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-module.exports = app
+module.exports = app;
