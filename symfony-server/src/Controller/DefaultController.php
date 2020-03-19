a9b58038ed7a8881d36cbf2879950dd71946874b
MPB <steelstring@protonmail.com>
2020-03-18 17:46:18 -0700

    added list of theaters to index page; prepended t_ on template variable names

--- symfony-server/src/Controller/DefaultController.php
+++ symfony-server/src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+// $t_variable names are for variables going into templates
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -10,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 define('TEMPLATES_DIR', __DIR__ . '/../../templates');
 define('LISTINGS_FILE', __DIR__ . '/../../../getListings/theaterData/listings.json');
+define('THEATER_FILE', __DIR__ . '/../../../getListings/theaterData/theaters.json');
 
 class DefaultController extends AbstractController {
   /**
@@ -19,23 +22,24 @@ class DefaultController extends AbstractController {
 
     $movieDate = date('Y-m-d');
 
-    $text = file_get_contents(LISTINGS_FILE);
-    $movieData = json_decode($text);
+    $t_theaters = json_decode(file_get_contents(THEATER_FILE));
+
+    $t_movieData = json_decode(file_get_contents(LISTINGS_FILE));
 
     // drop first (header) line
-    array_shift($movieData);
+    array_shift($t_movieData);
 
     // convert array of dates to comma separated string:
-    foreach ($movieData as &$row) {
+    foreach ($t_movieData as &$row) {
       $row[6] = implode(', ', $row[6]);
     }
 
-    $availableDates = array_column($movieData, 0);
-    $availableDates = array_unique($availableDates);
-    sort($availableDates);
+    $t_availableDates = array_column($t_movieData, 0);
+    $t_availableDates = array_unique($t_availableDates);
+    sort($t_availableDates);
     // remove dates before today
-    $availableDates = array_filter(
-      $availableDates,
+    $t_availableDates = array_filter(
+      $t_availableDates,
       function($v) use ($movieDate) {
         return $v >= $movieDate;
       }
